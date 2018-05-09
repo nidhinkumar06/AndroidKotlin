dc15954184a51ddce74f030a7d0edb1dccf4c52c
nidhinkumar <nidhinkumar@codingtown.com>
2018-05-09 19:56:28 +0530

    Initial Commit of KotlinChat App

---
+++ app/src/main/java/bytez/nidhin/kotlinchat/MainActivity.kt
@@ -0,0 +1,30 @@
+package bytez.nidhin.kotlinchat
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_people -> {
+                //TODO:Return People fragment
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_my_account -> {
+                //TODO:Return myaccount fragement
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+}
---
+++ app/src/main/java/bytez/nidhin/kotlinchat/SplashActivity.kt
@@ -0,0 +1,21 @@
+package bytez.nidhin.kotlinchat
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import org.jetbrains.anko.startActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+        if (FirebaseAuth.getInstance().currentUser == null)
+            startActivity<SignInActivity>()
+        else
+            startActivity<MainActivity>()
+        finish()
+    }
+}
